5b78db68e3e1b2dc409552ca1963c4e8d70510ad
mipsmonsta <yaojintham@gmail.com>
2021-06-03 23:52:53 +0800

    Fixed discount of pricing based on switch toggle

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 let priceMap = {
-  1: ["10K", "$8.00"],
-  2: ["50K", "$12.00"],
-  3: ["100K", "$16.00"],
-  4: ["500K", "$24.00"],
-  5: ["1M", "$35.00"],
+  1: ["10K", "$8.00", "$6.00"],
+  2: ["50K", "$12.00", "$9.00"],
+  3: ["100K", "$16.00", "$12.00"],
+  4: ["500K", "$24.00", "$18.00"],
+  5: ["1M", "$35.00", "$26.25"],
 };
+
+function setPrice(value) {
+  let pageViewElment = document.getElementById("page-view");
+  let amountElement = document.getElementById("amount");
+  let switchElement = document.querySelector(".discount-switch");
+
+  pageViewElment.textContent = priceMap[value][0];
+
+  if (switchElement.checked) {
+    amountElement.textContent = priceMap[value][2];
+  } else {
+    amountElement.textContent = priceMap[value][1];
+  }
+}
 
 document.getElementById("range-pageview").oninput = function () {
   const value = ((this.value - this.min) / (this.max - this.min)) * 100;
@@ -20,19 +34,12 @@ document.getElementById("range-pageview").oninput = function () {
   //console.log(this.style.background);
 
   //set the pageview and amount
-  document.getElementById("page-view").textContent = priceMap[this.value][0];
-  document.getElementById("amount").textContent = priceMap[this.value][1];
+  setPrice(this.value);
 };
 
 let switchElement = document.querySelector(".discount-switch");
-let discountElement = document.querySelector(".discount");
-discountElement.style.visibility = "hidden"; //hide by default when script first runs
 
 switchElement.addEventListener("change", () => {
-  // console.log(switchElement.checked);
-  if (switchElement.checked) {
-    discountElement.style.visibility = "visible";
-  } else {
-    discountElement.style.visibility = "hidden";
-  }
+  rangeElement = document.getElementById("range-pageview");
+  setPrice(rangeElement.value);
 });
